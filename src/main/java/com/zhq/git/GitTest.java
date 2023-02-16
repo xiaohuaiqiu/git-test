29f557c080672562fcfabeedff6c1ead528969fb
ZHQ <zhq@qq.com>
2023-02-16 22:34:44 +0800

    third commit

--- src/main/java/com/zhq/git/GitTest.java
+++ src/main/java/com/zhq/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.err.println("hello.git!");
         System.err.println("hello.git2!");
+        System.err.println("hello.git3!");
     }
 }
